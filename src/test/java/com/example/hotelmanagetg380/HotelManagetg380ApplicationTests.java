6cb94e289d259bce0aefa8482059b4ad9ffd7ad8
SaviduL <76526788+SaviduL@users.noreply.github.com>
2022-10-11 22:25:54 +0530

    initial structure

---
+++ src/test/java/com/example/hotelmanagetg380/HotelManagetg380ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelmanagetg380;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagetg380ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
